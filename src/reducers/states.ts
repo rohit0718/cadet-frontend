ebe23662441c63aa85341007ee51cb938df7b184
angelsl <angelsl@in04.sg>
2020-05-23 04:25:10 +0800

    Remove Chatkit

--- src/reducers/states.ts
+++ src/reducers/states.ts
@@ -347,7 +347,6 @@ export const defaultRoomId = null;
 export enum SideContentType {
   autograder = 'autograder',
   briefing = 'briefing',
-  chat = 'chat',
   dataVisualiser = 'data_visualiser',
   editorGrading = 'editor_grading',
   editorAutograder = 'editor_autograder',
